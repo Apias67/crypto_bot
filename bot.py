aae9305a39afe2d4b15189fffcbbfd8481e8ca89
Apias67 <apias2015@gmail.com>
2026-03-18 18:11:56 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import pandas as pd
 from websocket import WebSocketApp
 from telegram import Bot
 import requests
+import base64   # <- dodaj to, jeśli używasz Base64
 
 # =========================
 # CONFIG
